cd71e87721a281c12f32b5850c7331d622174225
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-08-11 14:41:29 +0530

    ADDED: Ability to select salary from employee payroll database

--- payroll_service.sql
+++ payroll_service.sql
@@ -9,7 +9,7 @@ desc employee_payroll;
 
 #UC3
 select * from employee_payroll;
-insert into employee_payroll (name,salary,startDate) values('Akhil',80000,'2022-8-25');
+insert into employee_payroll (name,salary,startDate) values('Akhil',80000,'2022-08-25');
 insert into employee_payroll (name,salary,startDate) values('Mohit',60000,'2021-10-2');
 insert into employee_payroll (name,salary,startDate) values('Rohit',50000,'2020-4-5');
 insert into employee_payroll (name,salary,startDate) values('Tina',70000,'2022-8-12');
@@ -17,3 +17,8 @@ insert into employee_payroll (name,salary,startDate) values('Parnika',60000,'202
 
 #UC4
 select * from employee_payroll;
+
+#UC5
+select * from employee_payroll where name="Akhil";
+select * from employee_payroll where salary=60000;
+select * from employee_payroll where startDate between '2022-08-25' And date(now());
