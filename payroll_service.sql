919272dc724c5b3613d16e5f0a48a8fa4882d859
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-08-11 15:10:37 +0530

    ADDED: Ability to extend payroll table to basic pay, deduction, TaxablePay, IncomeTax, NetPay

--- payroll_service.sql
+++ payroll_service.sql
@@ -38,10 +38,16 @@ select count(salary) from employee_payroll;
 set sql_safe_updates=0;
 
 #UC8
-#UC8
 alter table employee_payroll add phone_number varchar(13) after name;
 alter table employee_payroll add address varchar(200) after phone_number;
 alter table employee_payroll add department varchar(100) not null after address;
 alter table employee_payroll alter address set default 'TBD';
 insert into employee_payroll (id,name,phone_number,address,department,salary,startDate,gender) 
 values(8,'Rohit','9998887771','pune','IT', 70000, '2018-08-11','M');
+
+#UC9
+alter table employee_payroll rename column salary to basic_pay;
+alter table employee_payroll add deductions double not null after basic_pay;
+alter table employee_payroll add taxable_pay double not null after deductions;
+alter table employee_payroll add tax double not null after taxable_pay;
+alter table employee_payroll add net_pay double not null after tax;
